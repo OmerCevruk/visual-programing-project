4978eade6aa617be4430176caef20724529dcd10
Ardacld <109745271+aard-c@users.noreply.github.com>
2024-06-03 13:50:13 +0300

    attendance frame looks fine but cant see data

--- src/main/java/com/mycompany/mavenproject1/AttendanceFrame.java
+++ src/main/java/com/mycompany/mavenproject1/AttendanceFrame.java
@@ -1,45 +1,52 @@
 package com.mycompany.mavenproject1;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
 
 public class AttendanceFrame extends javax.swing.JFrame {
     private int parentID;
-    private JTable attendanceTable;
+
+ 
+    private Connection conn;
+
+    Vector<Vector<Object>> data = new Vector<>();
 
     public AttendanceFrame(int parentID) {
         this.parentID = parentID;
         initComponents();
-        loadAttendanceData();
+        initializeDatabaseConnection();
+        loadDataFromDatabase();
+        populateTable();
     }
 
-    
+    private void initializeDatabaseConnection() {
+        try {
+            conn = new JDBCPostgreSQLConnection().connect();
+        } catch (Exception e) {
+            Logger.getLogger(AttendanceFrame.class.getName()).log(Level.SEVERE, "Database connection failed", e);
+        }
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        attendanceTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null},
-                {null},
-                {null},
-                {null}
-            },
-            new String [] {
-                "Attendance Records"
-            }
+        attendanceTable.setModel(new DefaultTableModel(
+            new Object [][] {},
+            new String [] {"Date", "Status"}
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(attendanceTable);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -59,36 +66,71 @@ public class AttendanceFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
 
-    private void loadAttendanceData() {
-        try (Connection conn = new JDBCPostgreSQLConnection().connect()) {
-            String sql = "SELECT a.Date, a.Status " +
-                         "FROM attendance a " +
-                         "JOIN parent_child pc ON a.ChildID = pc.ChildID " +
-                         "WHERE pc.ParentID = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+private void loadDataFromDatabase() {
+        data = new Vector<>();
+        try (PreparedStatement pstmt = conn.prepareStatement(
+                "SELECT a.Date, a.Status " +
+                "FROM attendance a " +
+                "JOIN parent_child pc ON a.ChildID = pc.ChildID " +
+                "WHERE pc.ParentID = ?")) {
             pstmt.setInt(1, parentID);
             ResultSet rs = pstmt.executeQuery();
 
-            DefaultTableModel model = new DefaultTableModel(new String[]{"Date", "Status"}, 0);
             while (rs.next()) {
-                String date = rs.getString("Date");
-                String status = rs.getString("Status");
-                model.addRow(new Object[]{date, status});
+                Vector<Object> row = new Vector<>();
+                row.add(rs.getString("Date"));
+                row.add(rs.getString("Status"));
+                data.add(row);
             }
-            attendanceTable.setModel(model);
         } catch (SQLException ex) {
-            ex.printStackTrace();
+            Logger.getLogger(AttendanceFrame.class.getName()).log(Level.SEVERE, "Failed to load data", ex);
         }
+    }
+
+    private void populateTable() {
+        if (data.isEmpty()) {
+            Logger.getLogger(AttendanceFrame.class.getName()).log(Level.WARNING, "No data to display");
+            return;
+        }
+
+        Object[][] tableData = convertDataToTableFormat();
+        DefaultTableModel tableModel = createTableModel(tableData);
+
+        attendanceTable.setModel(tableModel);
+        tableModel.fireTableDataChanged();
+    }
+
+    private Object[][] convertDataToTableFormat() {
+        int rowCount = data.size();
+        int colCount = data.get(0).size();
+        Object[][] tableData = new Object[rowCount][colCount];
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                tableData[i][j] = data.get(i).get(j);
+            }
+        }
+
+        return tableData;
+    }
+
+    private DefaultTableModel createTableModel(Object[][] tableData) {
+        return new DefaultTableModel(tableData, new Object[]{"Date", "Status"}) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // All cells are non-editable
+            }
+        };
     }
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
-            new AttendanceFrame(1).setVisible(true); // Example parentID
+            new AttendanceFrame(5).setVisible(true); // Example parentID
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable attendanceTable;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
